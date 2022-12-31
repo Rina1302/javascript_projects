606e3f6b7cbecf72a67fb19351171432ec30058c
Rina <92846534+Rina1302@users.noreply.github.com>
2022-12-31 13:17:58 +0000

    app stage 1

---
+++ yourPlayground.js
@@ -0,0 +1,22 @@
+console.log('hello rina');
+console.log('rina pawar');
+//variables
+name = 'riana';
+console.log(name);
+food = 20;;
+console.log(food);
+
+//operators
+// fruit=prompt('what is yourfavourite fruite?')
+// console.log(fruit)
+food = Number(prompt('how much was the food?'))
+
+tipPercentage = prompt('tip you want give %?') / 100
+tipAmount = food * tipPercentage
+total=food + tipAmount
+alert(total)
+console.log(tipAmount)
+
+alert(tipAmount)
+console.log(total);
+//user 
