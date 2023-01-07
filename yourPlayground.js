26ee6d6488a1000513cebd80e3158f60eced7033
rinawatar <>
2023-01-07 16:36:11 +0000

    commit 2 food calci

--- yourPlayground.js
+++ yourPlayground.js
@@ -1,22 +1,33 @@
-console.log('hello rina');
-console.log('rina pawar');
-//variables
-name = 'riana';
-console.log(name);
-food = 20;;
-console.log(food);
-
+// console.log('hello rina');
+// console.log('rina pawar');
+// //variables
+// name = 'riana';
+// console.log(name);
+// food = 20;;
+// console.log(food);
+// console.log("hii rina")
 //operators
 // fruit=prompt('what is yourfavourite fruite?')
 // console.log(fruit)
-food = Number(prompt('how much was the food?'))
+// food = Number(prompt('how much was the food?'))
 
-tipPercentage = prompt('tip you want give %?') / 100
-tipAmount = food * tipPercentage
-total=food + tipAmount
-alert(total)
-console.log(tipAmount)
+// tipPercentage = prompt('tip you want give %?') / 100
+// tipAmount = food * tipPercentage
+// total=food + tipAmount
+// alert(total)
+// console.log(tipAmount)
 
-alert(tipAmount)
-console.log(total);
+// alert(tipAmount)
+// console.log(total);
 //user 
+food =prompt("how much food cost?")
+tipPercentage=Number(prompt("how much food tip you give %?")/100);
+tipAmount= food * tipPercentage
+total= number(tipAmount + food)
+console.log(tipAmount)
+console.log(total)
+
+
+
+
+
